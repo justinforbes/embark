justinforbes/embark

commit a35685ee4faf3e59db2264b0010ea4f8bc6e7a25

add writing to log func

embark/workers/update/update.py
@@ -18,7 +18,7 @@ from workers.orchestrator import get_orchestrator
 logger = logging.getLogger(__name__)
 
 
-def exec_blocking_ssh(client: SSHClient, command: str) -> str:
+def exec_blocking_ssh(client: SSHClient, command: str, log_write: function = None) -> str:
     """
     Executes ssh command blocking, as exec_command is non-blocking
     Warning: This command might block forever, if the output is too large (based on recv_exit_status). Thus redirect to file
@@ -33,6 +33,9 @@ def exec_blocking_ssh(client: SSHClient, command: str) -> str:
     status = stdout.channel.recv_exit_status()
     if status != 0:
         raise paramiko.ssh_exception.SSHException(f"Command failed with status {status}: {command}")
+
+    if log_write:
+        log_write(f"\nInput: {command} \nOutput: {stdout.read().decode().strip()} \nStatus: {str(status)}\n")
 
     return stdout.read().decode().strip()
 

embark/workers/models.py
@@ -4,6 +4,7 @@ __license__ = 'MIT'
 
 import os
 import ipaddress
+from pathlib import Path
 import socket
 import paramiko
 
@@ -137,6 +138,17 @@ class Worker(models.Model):
         null=True
     )
 
+    def write_log(self, string):
+        """
+        Writes into self.log_location
+        :returns: True/False
+        """
+        if not Path(self.log_location).is_file():
+            return False
+        with open(self.log_location,'w') as log_file:
+            log_file.writeln(string)
+        return True
+
     def clean(self):
         super().clean()
         if self.configurations.exists():
@@ -177,7 +189,7 @@ class Worker(models.Model):
 
         if ssh_client.get_transport() is None or not ssh_client.get_transport().is_active():
             raise paramiko.SSHException("Failed to connect to worker with any configuration.")
-
+        self.write_log(f"SSH Connection to {self.ip_address} established")
         return ssh_client
 
 

embark/workers/tasks.py
@@ -68,7 +68,7 @@ def update_system_info(worker: Worker):
     try:
         ssh_client = worker.ssh_connect(timeout=10)
 
-        os_info = exec_blocking_ssh(ssh_client, 'grep PRETTY_NAME /etc/os-release')
+        os_info = exec_blocking_ssh(ssh_client, 'grep PRETTY_NAME /etc/os-release', worker.write_log)
         os_info = os_info[len('PRETTY_NAME='):-1].strip('"')
 
         cpu_info = exec_blocking_ssh(ssh_client, 'nproc')

commit 0cc6739670108f7005cd744d7604120739fda061

fix: return stdout data properly in SSH calls
- fixes issue introduced with a35685ee4faf3e59db2264b0010ea4f8bc6e7a25
- call `read` only once as multiple calls are not possible
- fixes problem occuring when `log_write` is not `None`

Signed-off-by: Michael Guschlbauer <michael.guschlbauer@rohde-schwarz.com>

embark/workers/update/update.py
@@ -34,10 +34,12 @@ def exec_blocking_ssh(client: SSHClient, command: str, log_write: callable = Non
     if status != 0:
         raise paramiko.ssh_exception.SSHException(f"Command failed with status {status}: {command}")
 
-    if log_write:
-        log_write(f"\nInput: {command} \nOutput: {stdout.read().decode().strip()} \nStatus: {str(status)}\n")
+    stdout_str = stdout.read().decode().strip()
 
-    return stdout.read().decode().strip()
+    if log_write:
+        log_write(f"\nInput: {command} \nOutput: {stdout_str} \nStatus: {str(status)}\n")
+
+    return stdout_str
 
 
 def _copy_files(client: SSHClient, dependency: DependencyType, log_write: callable = None):